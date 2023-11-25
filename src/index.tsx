865f527dabb5c3bea03ee5e010ed7eed34e1b877
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-11-25 17:13:39 +0900

    tsxでimportできるようにした

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 // @ts-expect-error
 import rsdws from "react-server-dom-webpack/server";
 const { renderToPipeableStream } = rsdws;
-import { App } from "./app/App.js";
+import { App } from "./app/App.tsx";
 
 renderToPipeableStream(<App />).pipe(process.stdout);
